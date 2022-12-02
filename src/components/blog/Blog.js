c26f5335f933d300cbea904462cdb771144abf82
akshaymattoo <akshaymattoo@gmail.com>
2022-12-02 14:56:49 -0800

    updates for my portfolio

---
+++ src/components/blog/Blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Grid } from "@mui/material";
+function Blog() {
+  return (
+    <Box>
+      <Grid container display={"flex"} justifyContent={"center"} mt="12rem">
+        <h1>comming soon...</h1>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Blog;
