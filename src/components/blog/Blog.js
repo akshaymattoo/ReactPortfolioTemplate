ea491e8b7b1dd5ea23224ce6e731e9f6a9b22849
akshaymattoo <akshaymattoo@gmail.com>
2022-12-22 18:53:05 -0800

    updated with live feed on medium article

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,14 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BlogBlock from "./BlogBlock";
 import { Box, Grid } from "@mui/material";
 import { info } from "../../info/Info";
 
 export default function Blog() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    console.log("inside useEffect");
+    // declare the async data fetching function
+    const fetchData = async () => {
+      // get the data from the api
+      const data = await fetch(
+        "https://sparkling-bonus-3348.akshaymattoo.workers.dev/"
+      );
+      // convert the data to json
+      const json = await data.json();
+
+      json.data.posts.sort((a, b) => {
+        return b.createdAt - a.createdAt;
+      });
+      // set state with the result
+      setBlogs(json.data.posts);
+    };
+
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, []);
+
   // Here in useEffect i will have to go through all the posts and get them ina ayyasorted by createdAt
   return (
     <Box mt={"3rem"}>
       <Grid container display={"flex"} justifyContent={"center"} spacing={3}>
-        {info.blog.map((project, index) => (
+        {blogs.map((project, index) => (
           <Grid item key={index}>
             <BlogBlock
               image={project.imageUrl}
